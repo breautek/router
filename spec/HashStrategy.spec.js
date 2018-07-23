8a49976ccef1b80447186210735b268e86b2901e
Norman Breau <norman@normanbreau.com>
2018-07-23 13:23:30 -0300

    stashing

---
+++ spec/HashStrategy.spec.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// console.log(process.env);
+
+// import HashStrategy from '../src/HashStrategy.js';
+// import Router from '../src/Router.js';
+
+// jest.mock('../src/HashStrategy.js');
+// jest.mock('../src/Router.js');
+
+// it('test', () => {
+// 	expect(Router).not.toHaveBeenCalled();
+// 	expect(HashStrategy).not.toHaveBeenCalled();
+
+// 	var strategy = new HashStrategy(new Router());
+	
+// 	expect(HashStrategy).toHaveBeenCalledTimes(1);
+// });
+
+describe('test', () => {
+	it('test 1', () => {
+		expect(true).toBe(true);
+	});
+});
+
+
+// 'use strict';
+
+// console.log(process.env);
+
+// import HashStrategy from '../src/HashStrategy.js';
+// import Router from '../src/Router.js';
+
+// jest.mock('../src/HashStrategy.js');
+// jest.mock('../src/Router.js');
+
+// it('test', () => {
+// 	expect(Router).not.toHaveBeenCalled();
+// 	expect(HashStrategy).not.toHaveBeenCalled();
+
+// 	var strategy = new HashStrategy(new Router());
+	
+// 	expect(HashStrategy).toHaveBeenCalledTimes(1);
+// });
