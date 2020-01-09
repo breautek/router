bc50cd0b7e3ab964d5fbc4e01fe8290886a55eac
Tyler Breau <tylerbreau@hotmail.com>
2020-01-09 15:03:43 -0400

    removed unnecessary commented line

--- src/URLParser.js
+++ src/URLParser.js
@@ -15,7 +15,6 @@ export class URLParser {
         let patternParts = this._getParts(this._pattern);
 
         if ((!this._allowPartialMatch && parts.length !== patternParts.length) || url === '') {
-        // if (!this._allowPartialMatch && parts.length !== patternParts.length) {
             return null;
         }
 
