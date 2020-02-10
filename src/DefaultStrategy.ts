0dab8de355a5b5389e8222dea70fe84a6d05fd75
Norman Breau <norman@normanbreau.com>
2020-02-09 23:14:28 -0400

    docs

--- src/DefaultStrategy.ts
+++ src/DefaultStrategy.ts
@@ -2,4 +2,7 @@
 import {HashStrategy} from './HashStrategy';
 import { IRouterStrategyClass } from './IRouterStrategyClass';
 
+/**
+Alias for {@link HashStrategy}
+*/
 export let DefaultStrategy: IRouterStrategyClass = HashStrategy;
